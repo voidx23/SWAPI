b2be985d5648249cd13cef43abbbdcfef5e2a27c
Nihal Mohamed Bashir <nihalmb23@gmail.com>
2024-02-24 21:46:22 +0530

    dome some minor update on planet cards, text sizes , responsive issues, made minor changes in the design

--- src/sections/description/DescriptionSection.jsx
+++ src/sections/description/DescriptionSection.jsx
@@ -3,19 +3,17 @@ import './descriptionSection.css'
 
 function DescriptionSection() {
     return (
-        <div className='descrption-section m flex justify-center items-center flex-col'>
-            <div className="sec-1"> <h1 className='descrip-1 text-white'>Discover The Star Wars <br /> Universe.</h1></div>
+        <div className='descrption-section m flex justify-center flex-col'>
+            <div className="sec-1 "> <h1 className='descrip-1 text-white'>Discover The Star Wars <br /> Universe.</h1></div>
             <div className="sec-2-main flex">
-                <div className="sec-sub-3">
-                    <img src="../../../images/violetplanet.png" alt="" />
-                </div>
+
                 <div className="sec-sub-1">
-                    <h1 className='descrip-2 text-xl p-[5vw] mx-[1vw] lg:px-[5vw] lg:mx-[2vw] '>Welcome to our interactive planets directory! Dive into the fascinating world of Star Wars and discover
+                    <h1 className='descrip-2 text-3xl p-[4vw] mx-[1vw] lg:px-[5vw] lg:mx-[2vw] '>Welcome to our interactive planets directory! Dive into the fascinating world of Star Wars and discover
                         detailed information about each planet, including its climate, population, and terrain. Journey through the galaxy as you learn about
                         the diverse environments that shape the Star Wars universe.</h1>
                 </div>
                 <div className="sec-sub-2">
-                    <img className=' lg:w-[100vw]' src="../../../images/sateliteship.png" alt="" />
+                    <img className='sateliteship lg:w-[100vw]' src="../../../images/sateliteship.png" alt="" />
                 </div>
 
             </div>
